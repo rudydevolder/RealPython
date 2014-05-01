0911849f2da6c6299faccdb8ac0f7b4459bad51b
rudydevolder <rudy@devolder.be>
2014-05-01 15:13:20 +0200

    added Dynamic route

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ from flask import Flask
 app = Flask(__name__)
 
 #Use decorators to link to an URL:
+#Static routes:
 @app.route("/")
 @app.route("/hello")
 
 #define the view using a function, wich returns a string:
 def hello_world():
     return "Hello, World!"
+
+#Dynamic routes:
+@app.route("/test/<search_query>")
+def search(search_query):
+    return ("Test search:" + search_query)
+
 
 if __name__ == "__main__":
     app.run()
