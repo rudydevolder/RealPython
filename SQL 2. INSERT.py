1823b859075cace9b17829bb74f8bfcf598451e3
rudydevolder <rudy@devolder.be>
2014-05-01 16:50:33 +0200

    added SQL CREATE and INSERT

--- SQL 2. INSERT.py
+++ SQL 2. INSERT.py
@@ -5,10 +5,17 @@ conn = sqlite3.connect("new.db")
 
 cursor = conn.cursor()
 
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+try:
+    cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+    cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+    #cursor.execute("INSERT INTO populations VALUES('San Francisco', 'CA', 800000)") # => Error
 
-conn.commit()
+    conn.commit()
 
+except sqlite3.OperationalError:
+    print "Oops! Something went wrong. Try again..."
+    print "Database error : ", sqlite3.DatabaseError
+
+#Close the database:
 conn.close()
 
